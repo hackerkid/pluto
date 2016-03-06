aeccf025d43a27df862d6252c9a42f00c76e8747
Vishnu Ks <hackerkid@vishnuks.com>
2016-03-06 16:50:20 +0530

    Update lib.js

--- server/lib.js
+++ server/lib.js
@@ -4,7 +4,7 @@ var updates = levelup('./updates')
 
 function print(x)
 {
-	// console.log(x);
+	 console.log(x);
 }
 
 function get_last_message_id(id, callback)
@@ -27,7 +27,7 @@ function set_last_id(id,lastId, callback)
 {
 	db.put(id, lastId, function(err) {
 		if(err) {
-			// print("Unable to write to disk");
+			 print("Unable to write to disk");
 		}
 
 		callback();
@@ -50,34 +50,27 @@ function merge_the_new_update(update)
 			
 
 			if(err) {	
-				//// console.log("first");
-				//// console.log(obj.data);
 				var maxId = -1;
 
-				// print(obj.data.length);
 				for (var i = 0; i < obj.data.length; i++) {
 					if(maxId < obj.data[i].messageId) {
 						maxId = obj.data[i].messageId;
 					}
 				}
 
-				// print(maxId);
 
 				if(maxId != -1 && obj.data.length != 0) {
 					
 					set_last_id(id, maxId, function() {
 					
-						// console.log("set the maxid when nothing was present");
-					
+
 						updates.put(id, JSON.stringify(obj.data), function(err) {
 							
 							if(err) {
 								return // console.log("oops something went wrong while saving", err);
 							}
 
-							else {
-								// print("sucess");
-							}
+							
 						});
 					})
 				}
@@ -96,14 +89,10 @@ function merge_the_new_update(update)
 				get_last_message_id(id, function(lastId) {
 
 					for (var i = 0; i < obj.data.length; i++) {
-						// print("id is "+ obj.data[i].messageId)
-						
+
 						if(lastId < obj.data[i].messageId) {
-							// print("here");	
 							if(maxId < obj.data[i].messageId) {
-								// print("i am in");
 								maxId = obj.data[i].messageId;
-								// print("mad max is now " + maxId);
 							}
 
 							data.push(obj.data[i]);
@@ -111,14 +100,12 @@ function merge_the_new_update(update)
 
 						if(maxId != -1 && maxId > lastId) {
 							set_last_id(id, maxId, function() {
-								// print("maxid is set to "+ maxId);
 							})
 						}
 					}
 
 					var str = JSON.stringify(data);
 
-					// print(str);
 
 					updates.put(id, str, function(err) {	
 						if(err) {
@@ -137,7 +124,6 @@ function merge_the_new_update(update)
 
 
 
-// tested
 function fetch_updates(senderId, lastId, callback)
 {	
 	var obj = new Array();
@@ -147,15 +133,11 @@ function fetch_updates(senderId, lastId, callback)
 	final_res.senderId = senderId;
 	final_res.data = obj;
 
-	// console.log("here here here ");
-	
-	updates.get(senderId, function(err, res) {
-		//print("senderid is " + senderId + "lastid is " + lastId);
 
-		//// console.log(res);
+	updates.get(senderId, function(err, res) {
+
 
 		if(err) {
-			// print("error");
 
 			res = JSON.stringify(obj);
 			callback(res);
@@ -164,7 +146,6 @@ function fetch_updates(senderId, lastId, callback)
 		else {
 			data = JSON.parse(res);
 
-			//print(data.length);
 			for (var i = 0; i < data.length; i++) {
 				if(parseInt(data[i].messageId) > parseInt(lastId)) {
 					
@@ -175,8 +156,6 @@ function fetch_updates(senderId, lastId, callback)
 
 			final_res.data = obj;
 
-			//print("after filter ");
-			//// console.log( final_res);
 
 			var result = JSON.stringify(final_res);
 			callback(result);
